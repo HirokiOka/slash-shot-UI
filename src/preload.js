f9d93e88907d831e9ed7cfebae00e01cbe4ef80f
hiroka <mementoka@gmail.com>
2024-11-06 14:43:49 +0900

    Initial commit

---
+++ src/preload.js
@@ -0,0 +1,13 @@
+//Node API is available in the preload script
+window.addEventListener('DOMContentLoaded', () => {
+
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
